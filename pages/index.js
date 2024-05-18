b089b60b775ae6e943870e4ba0961b4ce944272c
smedinaxx <saul_medinax@hotmail.com>
2024-05-18 11:37:16 -0600

    add link

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Demo from '../pages/demo';
+import Link from 'next/link';
 
 const HomePage = () => {
   return (
     <div>
       <h1>Página de inicio</h1>
-      <a href='/demo'></a>
+      <Link href='/demo'>
+        ir a demo
+      </Link>
       <Demo /> {/* Aquí se renderiza el componente Demo */}
     </div>
   );
